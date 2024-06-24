4d8991d70dd0e36f2ab9f0a568c004488f83da1f
Krish Kapoor <kapoorkrish26@gmail.com>
2024-06-23 22:44:33 -0400

    2 Steppers

--- stepper.py
+++ stepper.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
-import time
 
 ################################
 # RPi and Motor Pre-allocations
@@ -14,14 +13,14 @@ motor2 = RpiMotorLib.A4988Nema(23, 24, (22,22,22), "DRV8825")
 # Actual motor control
 ###########################
 #
-start_time = time.time()
+
 motor1.motor_go(True, # False=Clockwise, True=Counterclockwise
                 "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
                 (200) * 5, # number of steps
                 .0005, # step delay [sec]
                 False, # True = print verbose output 
                 .05) # initial delay [sec]
-print(time.time() - start_time)
+
 motor2.motor_go(False, # False=Clockwise, True=Counterclockwise
                 "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
                 (200) * 5, # number of steps
